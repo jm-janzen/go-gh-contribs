8c12b1b653f39db38e2c1c2a1b68108c85dab338
JM <jmjanzen@gmail.com>
2016-07-30 13:28:54 -0400

    Init Github contribution tracker
    Just a simple API request on a given user name for now

---
+++ gh-contribs.go
@@ -0,0 +1,37 @@
+package main 
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "io"
+    "os"
+)
+
+func main() {
+    const BASE = "https://api.github.com/users/"
+    var name string = ""
+
+    if len(os.Args) < 1 {
+        fmt.Fprintf(os.Stderr, "Usage: ./gh-contribs [username]")
+        os.Exit(1)
+    } else {
+        name = os.Args[1]
+    }
+
+    var reqURI string = BASE + name
+    fmt.Fprintf(os.Stdout, "URI endpoint: %s\n", reqURI)
+
+    resp, err := http.Get(reqURI)
+
+    if err != nil {
+        log.Fatal(err)
+    } else {
+        defer resp.Body.Close()
+        _, err := io.Copy(os.Stdout, resp.Body)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+}
+
